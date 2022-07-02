3a9539eceb73d0b61918570dd0f7206cbfea746d
Millzy <millzyyt08@gmail.com>
2022-07-02 20:44:05 +0100

    main

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from "express";
+import PavlovAIC from "./pavlovAIC";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('Yo Millzy, you got the funni PSI-5 server up');
+});
+
+console.info("Initialising Pavlov.aic");
+
+const pavlov = new PavlovAIC("https://discord.com/api/webhooks/992821687869505586/wiz-dy_zuxDqV8hKCUmhdnkYWgvu1OPH6em52xFJimRSAZdwPyetsGViuyfbFhrbKZxo");
+
+app.listen(port, () => {
+    console.info(`PSI-5 Website is running at http://localhost:${port}`);
+});

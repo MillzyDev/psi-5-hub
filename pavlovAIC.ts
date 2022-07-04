8a38a4cba45898c3ab7a86b1bf01eb28e6dd8ee1
Millzy <millzyyt08@gmail.com>
2022-07-04 17:49:14 +0100

    fix

--- pavlovAIC.ts
+++ pavlovAIC.ts
@@ -5,13 +5,14 @@ import { WebhookClient, MessageEmbed } from "discord.js";
 class PavlovAIC {
     public client: WebhookClient;
 
-    public constructor(url: string) {
+    public constructor(url: any) {
         this.client = new WebhookClient({ url: url });
     }
 
     public sendMessage(embed: MessageEmbed) {
         this.client.send({
             username: "Pavlov.aic",
+            avatarURL: "https://cdn.discordapp.com/attachments/986042737956622366/993523600873234452/psi-5-outlined.png",
             embeds: [embed]
         });
     }

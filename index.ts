ba4f6f868fde3cdd4e691ce859fd78a20f496ca1
Millzy <millzyyt08@gmail.com>
2022-07-04 08:16:30 +0100

    yeet this

--- index.ts
+++ index.ts
@@ -16,8 +16,6 @@ app.get("/", (req: Request, res: Response) => {
 
 console.info("Initialising Pavlov.aic");
 
-const pavlov = new PavlovAIC("https://discord.com/api/webhooks/992821687869505586/wiz-dy_zuxDqV8hKCUmhdnkYWgvu1OPH6em52xFJimRSAZdwPyetsGViuyfbFhrbKZxo");
-
 app.listen(port, () => {
     console.info(`PSI-5 Website is running at http://localhost:${port}`);
 });
